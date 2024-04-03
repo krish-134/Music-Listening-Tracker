ce79260fd18e677f0b8f150d4657e19aa446e453
krish-134 <garchakri@gmail.com>
2024-04-02 20:11:37 -0700

    Phase 4: Task 3

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -6,14 +6,5 @@ public class Main {
     public static void main(String[] args) {
 
         new MusicTrackerGUI();
-
-        /*
-        try {
-            new MusicTrackerApp();
-        } catch (FileNotFoundException e) {
-            System.out.println("Unable to run application: file not found");
-        }
-
-         */
     }
 }
